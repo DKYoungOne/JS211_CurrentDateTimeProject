b6a9c9d9e23aec5ee153d869a71b7926f5bce84d
DKYoungOne <bawsinator@gmail.com>
2023-01-02 21:02:02 -0600

    Most functions and elements have funcionality

--- main.js
+++ main.js
@@ -30,21 +30,28 @@ const convertStringToNum = (string) => Number(string);
   // * NaN
   // * String
   
-
+const dataTypeIdentify = (data) => {
+  console.log(typeof data)
+  return typeof data;
+};
   
 // Write a JavaScript program that adds 2 numbers together.
 
-
+const add = (num1, num2) => Number(num1) + Number(num2);
 
 // Write a JavaScript program that runs only when 2 things are true.
-
-
-
 // Write a JavaScript program that runs when 1 of 2 things are true.
+// Write a JavaScript program that runs when both things are not true. 
+const noneTrue = (a, b) => !a && !b ? console.log('You do not have any fields filled') : console.log('error');
+const oneTrue = (a, b) => a || b ? console.log('one fieldfilled') : noneTrue(a, b);
+const twoTrue = (a, b) => a && b ? console.log('both fields filled') : oneTrue(a, b);
 
 
 
-// Write a JavaScript program that runs when both things are not true.  
+
+ 
+
+
 
 // ***************************
 //         PART TWO
@@ -58,7 +65,30 @@ const convertStringToNum = (string) => Number(string);
 // 6. go to `index.html` 
 // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
 
+//Addition and convert num to string demo, the convertstringtonum function is used in the original add function as well
+const addNumbers = (num1, num2) => {
+const sum = add(num1, num2)
+  document.getElementById('display-addition').innerHTML = 'Result: ' + String(sum);
+  console.log(sum);
+}
+const addForm = document.getElementById('addForm');
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const num1 = document.getElementById('num1').value;
+  console.log(num1);
+  const num2 = document.getElementById('num2').value;
+  console.log(num2);
+  addNumbers(num1, num2);
+});
 
+//Using the data type indentifier
+const whatIsThis = document.getElementById("whatIsThis");
+const dataTypeForm = document.getElementById('dataTypeForm');
+dataTypeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dataType = dataTypeIdentify(whatIsThis.value);
+  document.getElementById("display-data-type").innerHTML = dataType;
+})
 
 
 // Additional Resources
